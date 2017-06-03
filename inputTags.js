fe042b3c8d88e2fff12eaf7922159cba8da8713c
yanggf <2018708@qq.com>
2017-06-03 10:37:57 +0800

    Create inputTags.js

---
+++ inputTags.js
@@ -0,0 +1,18 @@
+
+/*
+ * inputTags
+ *
+ * @copyright funsent Inc.
+ * @link      http://www.funsent.com/libs/inputTags/
+ * @author    yanggf <2018708@qq.com>
+ * @version   0.99
+ */
+
+;(function($){
+
+    $.fn.inputTags = function(options){
+        var defaults = {};
+        var settings = $.extend({}, defaults, options);
+    };
+
+})(jQuery);
